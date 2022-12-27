169b2d970e138e8da68a1b4cc56b82a05609956b
Markiyan333 <markiyan333@gmail.com>
2022-12-27 17:55:54 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,27 +1,30 @@
-const inputText = document.querySelector('#validation-input');
+const inputText = document.querySelector('#validation-input')
 
-inputText.addEventListener('blur', onInputBlur);
-
-function onInputBlur(event, onInputBlurReset) {
-  if (event.currentTarget.value.length === Number(inputText.dataset.length)) {
-    inputText.classList.remove('invalid');
-    inputText.classList.add('valid');
-  }
-  else if (
-    event.currentTarget.value.length !== Number(inputText.dataset.length)) {
-    inputText.classList.remove('valid')
-    inputText.classList.add('invalid')
-    }
-}
+inputText.addEventListener('blur', event => {
+	inputText.classList.remove('valid','invalid')
+	if (event.target.value.length === Number(inputText.dataset.length)) {
+		inputText.classList.add('valid')
+	} else(
+		inputText.classList.add('invalid'))
+	
+})
 
 
+// const inputText = document.querySelector('#validation-input')
+// inputText.addEventListener('blur', onInputBlur);
 
-// addEventListener('blur', (event) => {
-// if (event.currentTarget.value.length === inputEl) {
-// inputText.classList.remove('invalid');
-// inputText.classList.add('valid');
-// } else {
-// inputText.classList.remove('valid');
-// inputText.classList.add('invalid');
+// con
+
+// function onInputBlur(event, onInputBlurReset) {
+//   if (event.currentTarget.value.length === Number(inputText.dataset.length)) {
+//     inputText.classList.remove('invalid');
+//     inputText.classList.add('valid');
+//   }
+//   else if (
+//     event.currentTarget.value.length !== Number(inputText.dataset.length)) {
+//     inputText.classList.remove('valid')
+//     inputText.classList.add('invalid')
+//     }
 // }
-// })
+
+
