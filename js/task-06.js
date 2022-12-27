40aadfd0d3572c2d9fd594c15d6e2708a00c2d41
Markiyan333 <markiyan333@gmail.com>
2022-12-27 13:39:35 +0200

    Update task js-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,27 @@
 const inputText = document.querySelector('#validation-input');
-const inputEl = Number(inputText.dataset.length);
 
-inputText.addEventListener('blur', (event) => {
-if (event.currentTarget.value.length === inputEl) {
+inputText.addEventListener('blur', onInputBlur);
+
+function onInputBlur(event, onInputBlurReset) {
+  if (event.currentTarget.value.length === Number(inputText.dataset.length)) {
     inputText.classList.remove('invalid');
     inputText.classList.add('valid');
-    } else {
-      inputText.classList.remove('valid');
-      inputText.classList.add('invalid');
+  }
+  else if (
+    event.currentTarget.value.length !== Number(inputText.dataset.length)) {
+    inputText.classList.remove('valid')
+    inputText.classList.add('invalid')
     }
-})
+}
+
+
+
+// addEventListener('blur', (event) => {
+// if (event.currentTarget.value.length === inputEl) {
+// inputText.classList.remove('invalid');
+// inputText.classList.add('valid');
+// } else {
+// inputText.classList.remove('valid');
+// inputText.classList.add('invalid');
+// }
+// })
